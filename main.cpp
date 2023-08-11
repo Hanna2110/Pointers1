06d10fc0ff4d9c844f26cb2af4a776b30a7364e5
HannaYehoshyna <25246437-HannaYehoshyna@users.noreply.replit.com>
2023-08-11 11:19:35 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //it is assigned a location in the computer’s memory
+  int myVar;
+  
+  //the identifier is used to refer to the variable
+  //the value of this variable is stored in the assigned location
+  myVar=10;
+
+  //the identifier is used to refer to the variable
+  cout<<"myVar = "<<myVar<<endl;   
+  
+  return 0;
+}
